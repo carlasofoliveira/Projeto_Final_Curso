5ba7860751bbd486331409ef5e5ff74698bc2a36
carlasofoliveira <58296811+carlasofoliveira@users.noreply.github.com>
2021-05-21 02:08:54 +0100

    criei o switch  e acrescentei um accordion

--- App_Center/adelementos/Novo_Evento.js
+++ App_Center/adelementos/Novo_Evento.js
@@ -2,17 +2,32 @@ import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
 import { StyleSheet, Text, View, TextInput, Switch } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
+import { List } from "react-native-paper";
 //import { Switch } from 'react-native-paper';
 
-export default function Novo_Evento({ navigation }) {
+export default function Novo_Evento() {
   const [input, setInput] = React.useState("");
-  const [isEnabled, setIsEnabled] = useState(false);
-  const alternarSwitch = () => {
-    setIsEnabled((previousState) => !previousState);
-  };
+  const [shouldShow, setShouldShow] = useState(false);
+
   const [isEnabled1, setIsEnabled1] = useState(false);
   const alternarSwitch1 = () => {
     setIsEnabled1((previousState) => !previousState);
+    setShouldShow(!shouldShow);
+  };
+
+  const [isEnabled2, setIsEnabled2] = useState(false);
+  const alternarSwitch2 = () => {
+    setIsEnabled2((previousState) => !previousState);
+  };
+
+  const [isEnabled3, setIsEnabled3] = useState(false);
+  const alternarSwitch3 = () => {
+    setIsEnabled3((previousState) => !previousState);
+  };
+
+  const [isEnabled4, setIsEnabled4] = useState(false);
+  const alternarSwitch4 = () => {
+    setIsEnabled4((previousState) => !previousState);
   };
 
   return (
@@ -42,19 +57,98 @@ export default function Novo_Evento({ navigation }) {
         placeholder="inserir designação..."
       />
 
-      <Switch
-        trackColor={{ false: "#808080", true: "#4F81C7" }}
-        thumbColor={!isEnabled ? "#ffffff" : "#FFFFFF"}
-        onValueChange={alternarSwitch}
-        value={isEnabled}
-      />
-      <Switch
-        trackColor={{ false: "#808080", true: "#4F81C7" }}
-        thumbColor={!isEnabled1 ? "#ffffff" : "#FFFFFF"}
-        onValueChange={alternarSwitch1}
-        value={isEnabled1}
-      />
+      <List.AccordionGroup>
+        <Text style={{ position: "relative" }}>
+          <List.Accordion title="Descrição" id="1">
+            <TextInput
+              style={styles.input}
+              onChangeText={(text) => setInput(text)}
+              onSubmitEditing={() => {
+                setInput("");
+              }}
+            />
+          </List.Accordion>
+        </Text>
+        <Text>
+          <List.Accordion title="Local" id="2">
+            <TextInput
+              style={styles.input}
+              onChangeText={(text) => setInput(text)}
+              onSubmitEditing={() => {
+                setInput("");
+              }}
+              placeholder="inserir designação..."
+            />
+          </List.Accordion>
+        </Text>
+      </List.AccordionGroup>
+
+      <View>
+        <View style={{ marginBottom: 28 }}>
+          <View style={{ width: 270 }}>
+            <Text style={{ fontSize: 15, marginTop: 6.02, left: 0.21 }}>
+              {"apresentar data/período"}
+            </Text>
+          </View>
+          <Switch
+            style={{ marginTop: -25, paddingBottom: -3 }}
+            trackColor={{ false: "#808080", true: "#4F81C7" }}
+            thumbColor={!isEnabled1 ? "#ffffff" : "#E5E5EA"}
+            onValueChange={alternarSwitch1}
+            value={isEnabled1}
+          />
+        </View>
+       
+
+        {shouldShow ? (
+          <View style={{ marginBottom: 28 }}>
+            <View style={{ width: 270 }}>
+              <Text style={{ fontSize: 15, marginTop: 6.02, left: 0.21 }}>
+                {"convidar parceiros"}
+              </Text>
+            </View>
+            <Switch
+              style={{ marginTop: -25 }}
+              trackColor={{ false: "#808080", true: "#4F81C7" }}
+              thumbColor={!isEnabled2 ? "#ffffff" : "#FFFFFF"}
+              onValueChange={alternarSwitch2}
+              value={isEnabled2}
+            />
+          </View>
+        ) : (
+          true
+        )}
+
+        <View style={{ marginBottom: 28 }}>
+          <View style={{ width: 270 }}>
+            <Text style={{ fontSize: 15, marginTop: 6.02, left: 0.21 }}>
+              {"solicitar recursos"}
+            </Text>
+          </View>
+          <Switch
+            style={{ marginTop: -25 }}
+            trackColor={{ false: "#808080", true: "#4F81C7" }}
+            thumbColor={!isEnabled3 ? "#ffffff" : "#FFFFFF"}
+            onValueChange={alternarSwitch3}
+            value={isEnabled3}
+          />
+        </View>
+        <View style={{ marginBottom: 28 }}>
+          <View style={{ width: 270 }}>
+            <Text style={{ fontSize: 15, marginTop: 6.02, left: 0.21 }}>
+              {"solicitar voluntários"}
+            </Text>
+          </View>
+          <Switch
+            style={{ marginTop: -25 }}
+            trackColor={{ false: "#808080", true: "#4F81C7" }}
+            thumbColor={!isEnabled4 ? "#ffffff" : "#FFFFFF"}
+            onValueChange={alternarSwitch4}
+            value={isEnabled4}
+          />
+        </View>
       </View>
+    </View>
   );
 }
 
@@ -64,15 +158,13 @@ const styles = StyleSheet.create({
     backgroundColor: "#EEF5FF",
     alignItems: "center",
     justifyContent: "center",
-    marginBottom:25,
-    marginTop:-305,
   },
   content: {
     textAlign: "center",
   },
 
   Imageinsert: {
-    paddingTop: 100,
+    paddingTop: -25,
     paddingHorizontal: 30,
   },
   box: {
@@ -131,9 +223,5 @@ const styles = StyleSheet.create({
     marginTop: 5,
     marginLeft: 30,
     paddingLeft: -5,
-  },
-  switch_button: {
-    fontSize: 16,
-    lineHeight: 25,
   },
 });
